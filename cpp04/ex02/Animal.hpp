6662bf8f72411b615f08450f80f08a5cc012081b
Anastasiia Pechkovska <167317438+Stacy314@users.noreply.github.com>
2025-09-24 15:26:52 +0000

    feat: add cpp04/ex01-02

--- cpp04/ex02/Animal.hpp
+++ cpp04/ex02/Animal.hpp
@@ -1,14 +1,23 @@
 #ifndef ANIMAL_HPP
 #define ANIMAL_HPP
+
 #include <iostream>
 #include <string>
+
 class Animal {
+public:
+    Animal();
+    Animal(const std::string& t);
+    Animal(const Animal&);
+    Animal& operator=(const Animal&);
+    virtual ~Animal();
+
+    virtual void makeSound() const = 0;
+    const std::string getType() const;
+
 protected:
     std::string type;
-public:
-    Animal(); Animal(const std::string&); Animal(const Animal&); Animal& operator=(const Animal&);
-    virtual ~Animal();
-    virtual void makeSound() const = 0; // pure
-    std::string getType() const;
+
 };
+
 #endif
--- cpp04/ex02/Cat.hpp
+++ cpp04/ex02/Cat.hpp
@@ -2,11 +2,21 @@
 #define CAT_HPP
 #include "Animal.hpp"
 #include "Brain.hpp"
+
 class Cat : public Animal {
+public:
+    Cat();
+    Cat(const Cat&);
+    Cat& operator=(const Cat&);
+    virtual ~Cat();
+	
+    virtual void makeSound() const;	
+
+    void setIdea(int i, const std::string& s);
+    const std::string& getIdea(int i) const;
+
 private:
     Brain* brain;
-public:
-    Cat(); Cat(const Cat&); Cat& operator=(const Cat&); virtual ~Cat();
-    virtual void makeSound() const;
 };
+
 #endif
